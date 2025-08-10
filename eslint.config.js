abe2a1a6eddd4b5714cb8a660281cd40638ebfb0
gkoos <gabor.koos@gmail.com>
2025-08-10 20:56:56 +0100

    TS linting fixed

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,32 @@
+// eslint.config.js
 import js from '@eslint/js'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.ts'],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        fetch: 'readonly',
+        RequestInfo: 'readonly',
+        URL: 'readonly',
+        RequestInit: 'readonly',
+        Response: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
     },
     rules: {
-      'no-unused-vars': 'warn',
+      ...ts.configs.recommended.rules,
+      'no-undef': 'off', // TS already checks globals
     },
   },
 ]
